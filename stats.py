bb7d8ce2ab4ea8aba4cc5338456e04cda3d0ecdd
AlexRFarnes <alexrfarnes@gmail.com>
2025-04-11 12:19:25 +0800

    Add the sorting function

--- stats.py
+++ stats.py
@@ -13,3 +13,19 @@ def get_num_chars(text):
                 char_count[char] = 1
 
     return char_count
+
+def sort_on(dict):
+    return dict["count"]
+
+def get_sorted_chars(chars):
+    sorted_chars = []
+    for key in chars:
+        char = key
+        count = chars[key]
+        sorted_chars.append({"char": char, "count": count})
+    
+    sorted_chars.sort(reverse=True, key=sort_on)
+
+    return sorted_chars
+
+
